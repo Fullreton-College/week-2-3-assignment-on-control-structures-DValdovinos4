739779d1075bf09c585956659ba49cccf735d5eb
DValdovinos4 <domanikvaldovinos@gmail.com>
2026-02-16 21:05:51 -0800

    Update SoftwareStore.cpp

--- SoftwareStore.cpp
+++ SoftwareStore.cpp
@@ -1,16 +1,43 @@
 #include <iostream>
 #include <iomanip>
+
 using namespace std;
-int main()
-{
-    double mph = 6.5;
-    // PACE calculation
-    /* write your code */
 
-    //express pace in minutes and seconds
-  /* write your code here */
+int main() {
+   int units;
+   double packageprice=199;
+   
+    cout << "Enter number of units purchased: ";
+    cin >> units;
 
-  //express your output in 1 decimal points
+    double Total = units * packageprice;
+    cout << fixed << setprecision(2);
+    
+    if (units <= 0)
+    {
+        cout << "Invalid input. Please enter a different amount of units" << endl;
+    }
+    else if (units >=1 && units <=9)
+    {
+        cout << "The total cost is: $" << Total << endl;
+    }
+    else if (units >=10 && units <=19)
+    {
+        cout << "The total cost is: $" << Total -( Total * 0.20) << endl;
+    }
+    else if (units >=20 && units <=49)
+    {
+        cout << "The total cost is: $" << Total -( Total * 0.30) << endl;
+    }
+    else if (units >=50 && units <=99)
+    {
+        cout << "The total cost is: $" << Total -( Total * 0.40) << endl;
+    }
+    else if (units >=100)
+    {
+        cout << "The total cost is: $" << Total -( Total * 0.50) << endl;
+    }
+    
 
-
+    return 0;
 }
