6785638cfa471fc48268df4e35555f1190b09e0d
DValdovinos4 <domanikvaldovinos@gmail.com>
2026-02-16 21:05:07 -0800

    Update Assignment1_RectangleArea.cpp

--- Assignment1_RectangleArea.cpp
+++ Assignment1_RectangleArea.cpp
@@ -8,29 +8,29 @@ int main() {
     cout << "Enter the width of the first rectangle: ";
     cin >> width;
 
-    cout << " Enter the heigh of the first rectangle: ";
+    cout << "Enter the height of the first rectangle: ";
     cin >> height; 
 
-    cout << " Enter the width of the second rectangle: ";
+    cout << "Enter the width of the second rectangle: ";
     cin >> width2;
 
-    cout << " Enter the heigh of the second rectangle: "; 
+    cout << "Enter the height of the second rectangle: "; 
     cin >> height2;
 
     int area = width * height;
 
     int area2 = width2 * height2;
 
-    cout << " The area of the first rectangle is: " << area << endl;
+    cout << "The area of the first rectangle is: " << area << endl;
 
-    cout << " The area of the second rectangle is: " << area2 << endl;
+    cout << "The area of the second rectangle is: " << area2 << endl;
 
     if (area > area2) {
-        cout << " The first reactangle has the greater area." << endl;
+        cout << "The first rectangle has the greater area." << endl;
     } else if (area < area2) {
-        cout << " The second rectangle has the greater area." << endl;
+        cout << "The second rectangle has the greater area." << endl;
     } else if (area == area2) {
-        cout << " The areas are the same" << endl;
+        cout << "The areas are the same." << endl;
     }
 
     
